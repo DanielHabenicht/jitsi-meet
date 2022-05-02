33bf1b8a033795152dbc997dec122c6228162491
DanielHabenicht <daniel.habenicht@t-systems.com>
2022-05-02 10:09:06 +0000

    fix linting error

--- react/features/shared-iframe/actionTypes.js
+++ react/features/shared-iframe/actionTypes.js
@@ -1,19 +1,19 @@
-// @flow
-
 /**
  * The type of the action which update the state without any side effects.
  *
+ * @example
  * {
  *     type: ONLY_UPDATE_SHARED_IFRAME_STATUS,
  *     status: string
  * }
  */
- export const ONLY_UPDATE_SHARED_IFRAME_STATUS = 'ONLY_UPDATE_SHARED_IFRAME_STATUS';
+export const ONLY_UPDATE_SHARED_IFRAME_STATUS = 'ONLY_UPDATE_SHARED_IFRAME_STATUS';
 
 /**
  * The type of the action which signals to update the current known state of the
  * shared iframe.
  *
+ * @example
  * {
  *     type: SET_SHARED_IFRAME_STATUS,
  *     status: string
@@ -25,6 +25,7 @@ export const SET_SHARED_IFRAME_STATUS = 'SET_SHARED_IFRAME_STATUS';
  * The type of the action which signals to reset the current known state of the
  * shared IFRAME.
  *
+ * @example
  * {
  *     type: RESET_SHARED_IFRAME_STATUS,
  * }
@@ -35,6 +36,7 @@ export const RESET_SHARED_IFRAME_STATUS = 'RESET_SHARED_IFRAME_STATUS';
  * The type of the action which signals to disable or enable the shared iframe
  * button.
  *
+ * @example
  * {
  *     type: SET_DISABLE_SHARED_IFRAME_BUTTON
  * }
